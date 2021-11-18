e9ae70831a146b8a45fd118a5123f717fd2de240
KambleKomal <{ID}+{username}@users.noreply.github.com>
2021-11-18 19:11:06 +0530

    javascript

--- javasript/calc.js
+++ javasript/calc.js
@@ -1,4 +1,4 @@
-function add(){
+/*function add(){
     var a = document.getElementById('first').value;
     var b = document.getElementById('second').value;
     var out = Number(a)+Number(b)
@@ -11,4 +11,22 @@ function sub(){
     var b = document.getElementById('second').value;
     var out = Number(a)-Number(b)
     document.getElementsByClassName('output')[0].innerText="Subtraction is "+out
+}*/
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  // console.log(hexColor);
+
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
 }
